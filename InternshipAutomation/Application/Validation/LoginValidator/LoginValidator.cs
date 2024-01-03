374c333fd9f3d05f42fc11bc587f6016c2f94998
eyupkahraman <eyupkhrmn45@gmail.com>
2024-01-03 18:08:59 +0300

    forgot password alanı validator eklendi.

--- InternshipAutomation/Application/Validation/LoginValidator/LoginValidator.cs
+++ InternshipAutomation/Application/Validation/LoginValidator/LoginValidator.cs
@@ -1,5 +1,6 @@
 using FluentValidation;
 using InternshipAutomation.Persistance.CQRS.Login;
+using InternshipAutomation.Persistance.CQRS.User;
 
 namespace InternshipAutomation.Application.Validation.LoginValidator;
 
@@ -9,5 +10,13 @@ public class LoginValidator : AbstractValidator<LoginCommand>
     {
         RuleFor(_ => _.UserName).NotEmpty().NotNull();
         RuleFor(_ => _.Password).NotEmpty().NotNull();
+    }
+}
+
+public class ForgotPasswordValidator : AbstractValidator<ForgotPasswordCommand>
+{
+    public ForgotPasswordValidator()
+    {
+        RuleFor(_ => _.UserCode).NotEmpty().NotNull();
     }
 }
